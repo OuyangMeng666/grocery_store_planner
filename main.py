9cac489096f2b981c69f249f69a1fed7f736bd34
OuyangMeng666 <43389574+OuyangMeng666@users.noreply.github.com>
2020-09-12 23:02:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ class User():
 
 
 bot = telepot.Bot('1187352721:AAEU2YwIDfvVD9xO2MskgwAFR4gGHyGlZds')
-bot.setWebhook("https://https://shopperlistone.herokuapp.com/{}".format(secret), max_connections=1)
+bot.setWebhook("https://shopperlistone.herokuapp.com/", max_connections=1)
 
 users = {}
 
